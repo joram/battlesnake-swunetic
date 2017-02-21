6a53e9a084cf0fd20e5336bf7f9766ddb6b496cf
John Oram <john@sendwithus.com>
2017-02-20 21:16:22 -0800

    run 100 games with the same snakes

--- src/main.go
+++ src/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -24,7 +25,40 @@ func main() {
 		http.ListenAndServe(":"+port, nil)
 	} else {
 		log.Println("Simulate a game to train swunetics!")
-		game := NewGame(2, 20)
-		game.Run()
+		Train(4, 100)
+
 	}
+}
+
+func Train(numSnakes, numGamesPerGeneration int) {
+	game := NewGame(numSnakes, 1)
+	trainingSnakes := game.currentGameState.HeuristicSnakes
+	trainingSnakes = MutateSnakes(trainingSnakes)
+	gamesWon := map[string]int{}
+	for _, snake := range trainingSnakes {
+		gamesWon[snake.Id] = 0
+	}
+
+	for i := 0; i < numGamesPerGeneration; i++ {
+		game := NewGame(len(trainingSnakes), 1)
+		game.currentGameState.HeuristicSnakes = trainingSnakes
+		game.Run()
+		for _, winner := range game.currentGameState.winners {
+			gamesWon[winner.Id] += 1
+		}
+
+		s := ""
+		for _, snake := range trainingSnakes {
+			s += fmt.Sprint("%v:%v\t", snake.Id, gamesWon[snake.Id])
+		}
+	}
+}
+
+func MutateSnakes(snakes []HeuristicSnake) []HeuristicSnake {
+	mutationAmount := []int{0, 5, 10, 15, 20, 20, 20, 20, 20, 20, 20, 20}
+
+	for i := 0; i < len(snakes); i++ {
+		snakes[i].Mutate(mutationAmount[i])
+	}
+	return snakes
 }
