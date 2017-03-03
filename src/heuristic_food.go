8b4e59c3d8644cf016267f3fc1ddb2a15521a03c
John Oram <john@sendwithus.com>
2017-03-03 12:53:44 -0800

    skip food if it will starve other snakes

--- src/heuristic_food.go
+++ src/heuristic_food.go
@@ -5,5 +5,34 @@ func NearestFoodHeuristic(gameState *GameState) WeightedDirections {
 	for _, food := range gameState.Food {
 		foods = append(foods, &food)
 	}
+
+	allOtherSnakesWillStarve := true
+	for _, snake := range gameState.OtherSnakes() {
+		canGetToFood := false
+		for _, food := range gameState.Food {
+			if gameState.aStar[snake.Id].turnsTo[food] < snake.HealthPoints {
+				canGetToFood = true
+				break
+			}
+		}
+		if canGetToFood {
+			allOtherSnakesWillStarve = false
+			break
+		}
+	}
+
+	if allOtherSnakesWillStarve {
+		healthiestSnake := true
+		for _, snake := range gameState.OtherSnakes() {
+			if snake.HealthPoints >= gameState.MySnake().HealthPoints {
+				healthiestSnake = false
+				break
+			}
+		}
+		if healthiestSnake {
+			return WeightedDirections{}
+		}
+	}
+
 	return MoveTo(gameState, foods)
 }
