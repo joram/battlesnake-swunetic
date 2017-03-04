c439efc8af93ebbc8a41030932abf66d3a5c5c6c
John Oram <john@sendwithus.com>
2017-03-03 16:21:39 -0800

    parallelize board control heuristic

--- src/heuristic_control.go
+++ src/heuristic_control.go
@@ -1,6 +1,9 @@
 package main
 
-import "math"
+import (
+	"math"
+	"sync"
+)
 
 func BoardControlHeuristic(gameState *GameState) WeightedDirections {
 	mySnake := gameState.MySnake()
@@ -9,21 +12,42 @@ func BoardControlHeuristic(gameState *GameState) WeightedDirections {
 	}
 
 	head := mySnake.Coords[0]
-	controlLeft := BoardControl(gameState, head.Left())
-	controlRight := BoardControl(gameState, head.Right())
-	controlUp := BoardControl(gameState, head.Up())
-	controlDown := BoardControl(gameState, head.Down())
-	maxControl := math.Max(controlLeft, math.Max(controlRight, math.Max(controlUp, controlDown)))
-	minControl := math.Min(controlLeft, math.Min(controlRight, math.Min(controlUp, controlDown)))
+	wg := sync.WaitGroup{}
+	control := map[string]float64{
+		UP:    0,
+		DOWN:  0,
+		LEFT:  0,
+		RIGHT: 0,
+	}
+	for dir, _ := range control {
+		wg.Add(1)
+		go func() {
+			newHead := head.Add(directionVector(dir))
+			control[dir] = BoardControl(gameState, &newHead)
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+
+	maxControl := float64(-1)
+	minControl := float64(-1)
+	for _, val := range control {
+		if maxControl == -1 || val > maxControl {
+			maxControl = val
+		}
+		if minControl == -1 || val < minControl {
+			minControl = val
+		}
+	}
 	if maxControl == minControl {
 		return []WeightedDirection{}
 	}
 
 	weightedDirections := []WeightedDirection{
-		{Weight: int(controlLeft / maxControl * 100), Direction: LEFT},
-		{Weight: int(controlRight / maxControl * 100), Direction: RIGHT},
-		{Weight: int(controlUp / maxControl * 100), Direction: UP},
-		{Weight: int(controlDown / maxControl * 100), Direction: DOWN},
+		{Weight: int(control[LEFT] / maxControl * 100), Direction: LEFT},
+		{Weight: int(control[RIGHT] / maxControl * 100), Direction: RIGHT},
+		{Weight: int(control[UP] / maxControl * 100), Direction: UP},
+		{Weight: int(control[DOWN] / maxControl * 100), Direction: DOWN},
 	}
 
 	return weightedDirections
